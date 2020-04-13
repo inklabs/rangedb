d45939ae286906ae3a9ef50a2b75345beb90d7e0
Jamie Isaacs <pdt256@gmail.com>
2020-04-13 11:36:21 -0700

    Added default value tests for paging, releasing v0.2.4

--- pkg/paging/paging_test.go
+++ pkg/paging/paging_test.go
@@ -1,6 +1,7 @@
 package paging_test
 
 import (
+	"net/url"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -45,4 +46,47 @@ func TestNewLinks_HasPreviousAndNoNextPage(t *testing.T) {
 	// Then
 	assert.Equal(t, "/e/thing?itemsPerPage=10&page=1", links.Previous)
 	assert.Equal(t, "", links.Next)
+}
+
+func TestNewPagination_ReturnsDefaultValueForItemsPerPage(t *testing.T) {
+	// When
+	pagination := paging.NewPagination(0, 0)
+
+	// Then
+	assert.Equal(t, paging.DefaultItemsPerPage, pagination.ItemsPerPage)
+	assert.Equal(t, paging.DefaultPage, pagination.Page)
+}
+
+func TestNewPagination_ReturnsMaxValueForItemsPerPage(t *testing.T) {
+	// When
+	pagination := paging.NewPagination(paging.MaxItemsPerPage+1, 1)
+
+	// Then
+	assert.Equal(t, paging.MaxItemsPerPage, pagination.ItemsPerPage)
+}
+
+func TestNewPaginationFromQuery(t *testing.T) {
+	// Given
+	values := url.Values{}
+	values.Set("itemsPerPage", "2")
+	values.Set("page", "3")
+
+	// When
+	pagination := paging.NewPaginationFromQuery(values)
+
+	// Then
+	assert.Equal(t, 2, pagination.ItemsPerPage)
+	assert.Equal(t, 3, pagination.Page)
+}
+
+func TestNewPaginationFromQuery_UsingDefaultValues(t *testing.T) {
+	// Given
+	values := url.Values{}
+
+	// When
+	pagination := paging.NewPaginationFromQuery(values)
+
+	// Then
+	assert.Equal(t, paging.DefaultItemsPerPage, pagination.ItemsPerPage)
+	assert.Equal(t, paging.DefaultPage, pagination.Page)
 }
--- store.go
+++ store.go
@@ -5,7 +5,7 @@ import (
 	"github.com/inklabs/rangedb/pkg/paging"
 )
 
-const Version = "0.2.4-dev"
+const Version = "0.2.4"
 
 // Record contains event data and metadata.
 type Record struct {
