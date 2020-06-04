751eedc94e3366837e6a6c735fdc96ac5184615f
Jamie Isaacs <pdt256@gmail.com>
2020-06-04 12:17:44 -0700

    Added BDD testing package

---
+++ rangedbtest/bdd/bdd.go
@@ -0,0 +1,131 @@
+package bdd
+
+import (
+	"context"
+	"reflect"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/inklabs/rangedb"
+)
+
+type Command interface {
+	rangedb.AggregateMessage
+	CommandType() string
+}
+
+type CommandDispatcher func(command Command)
+
+type TestCase struct {
+	store          rangedb.Store
+	dispatch       CommandDispatcher
+	previousEvents []rangedb.Event
+	command        Command
+}
+
+func New(store rangedb.Store, commandDispatcher CommandDispatcher) *TestCase {
+	return &TestCase{
+		store:    store,
+		dispatch: commandDispatcher,
+	}
+}
+
+func (c *TestCase) Given(events ...rangedb.Event) *TestCase {
+	c.previousEvents = events
+	return c
+}
+
+func (c *TestCase) When(command Command) *TestCase {
+	c.command = command
+	return c
+}
+
+func (c *TestCase) Then(expectedEvents ...rangedb.Event) func(*testing.T) {
+	return func(t *testing.T) {
+		t.Helper()
+
+		streamPreviousEventCounts := make(map[string]uint64)
+		for _, event := range c.previousEvents {
+			streamPreviousEventCounts[rangedb.GetEventStream(event)]++
+			require.NoError(t, c.store.Save(event, nil))
+		}
+
+		c.dispatch(c.command)
+
+		streamExpectedEvents := make(map[string][]rangedb.Event)
+		for _, event := range expectedEvents {
+			stream := rangedb.GetEventStream(event)
+
+			streamExpectedEvents[stream] = append(streamExpectedEvents[stream], event)
+		}
+
+		ctx := context.Background()
+		for stream, expectedEventsInStream := range streamExpectedEvents {
+			eventNumber := streamPreviousEventCounts[stream]
+			actualEvents := eventChannelToSlice(c.store.EventsByStreamStartingWith(ctx, eventNumber, stream))
+
+			assert.Equal(t, expectedEventsInStream, actualEvents, "stream: %s", stream)
+		}
+	}
+}
+
+func (c *TestCase) ThenInspectEvents(f func(t *testing.T, events []rangedb.Event)) func(t *testing.T) {
+	return func(t *testing.T) {
+		t.Helper()
+
+		streamPreviousEventCounts := make(map[string]uint64)
+		for _, event := range c.previousEvents {
+			streamPreviousEventCounts[rangedb.GetEventStream(event)]++
+			require.NoError(t, c.store.Save(event, nil))
+		}
+
+		c.dispatch(c.command)
+
+		ctx := context.Background()
+		var events []rangedb.Event
+		for _, stream := range getStreamsFromStore(c.store) {
+			eventNumber := streamPreviousEventCounts[stream]
+			actualEvents := eventChannelToSlice(c.store.EventsByStreamStartingWith(ctx, eventNumber, stream))
+
+			events = append(events, actualEvents...)
+		}
+
+		f(t, events)
+	}
+}
+
+func getStreamsFromStore(store rangedb.Store) []string {
+	streams := make(map[string]struct{})
+	for record := range store.EventsStartingWith(context.Background(), 0) {
+		streams[rangedb.GetStream(record.AggregateType, record.AggregateID)] = struct{}{}
+	}
+
+	keys := make([]string, 0, len(streams))
+	for k := range streams {
+		keys = append(keys, k)
+	}
+	return keys
+}
+
+func eventChannelToSlice(records <-chan *rangedb.Record) []rangedb.Event {
+	var events []rangedb.Event
+
+	for record := range records {
+		events = append(events, eventAsValue(record.Data))
+	}
+
+	return events
+}
+
+func eventAsValue(inputEvent interface{}) rangedb.Event {
+	var event rangedb.Event
+	reflectedValue := reflect.ValueOf(inputEvent)
+
+	if reflectedValue.Kind() == reflect.Ptr {
+		event = reflectedValue.Elem().Interface().(rangedb.Event)
+	}
+
+	return event
+}
---
+++ rangedbtest/bdd/bdd_test.go
@@ -0,0 +1,114 @@
+package bdd_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/inklabs/rangedb"
+	"github.com/inklabs/rangedb/provider/inmemorystore"
+	"github.com/inklabs/rangedb/rangedbtest/bdd"
+)
+
+func TestTestCase_Then(t *testing.T) {
+	t.Run("no events emitted", func(t *testing.T) {
+		// Given
+		store := inmemorystore.New()
+		testCase := bdd.New(store, noopDispatcher)
+		testCase.Given()
+		testCase.When(NoopCommand{})
+
+		// When
+		tt := testCase.Then()
+
+		// Then
+		t.Run("test case", tt)
+	})
+
+	t.Run("no events emitted, with previous events", func(t *testing.T) {
+		// Given
+		store := inmemorystore.New()
+		testCase := bdd.New(store, noopDispatcher)
+		testCase.Given(NoopEvent{ID: "xyz"})
+		testCase.When(NoopCommand{})
+
+		// When
+		tt := testCase.Then()
+
+		// Then
+		t.Run("test case", tt)
+	})
+
+	t.Run("dispatcher emits single event", func(t *testing.T) {
+		// Given
+		store := inmemorystore.New()
+		store.Bind(NoopEvent{})
+		dispatcher := stubEventDispatcher(store, NoopEvent{ID: "xyz"})
+		testCase := bdd.New(store, dispatcher)
+		testCase.Given()
+		testCase.When(NoopCommand{})
+
+		// When
+		tt := testCase.Then(NoopEvent{ID: "xyz"})
+
+		// Then
+		t.Run("test case", tt)
+	})
+}
+
+func TestTestCase_ThenInspectEvents(t *testing.T) {
+	t.Run("no events emitted", func(t *testing.T) {
+		// Given
+		store := inmemorystore.New()
+		testCase := bdd.New(store, noopDispatcher)
+		testCase.Given()
+		testCase.When(NoopCommand{})
+
+		// When
+		tt := testCase.ThenInspectEvents(func(t *testing.T, events []rangedb.Event) {
+			assert.Equal(t, 0, len(events))
+		})
+
+		// Then
+		t.Run("test case", tt)
+	})
+
+	t.Run("no events emitted, with previous events", func(t *testing.T) {
+		// Given
+		store := inmemorystore.New()
+		testCase := bdd.New(store, noopDispatcher)
+		testCase.Given(NoopEvent{ID: "xyz"})
+		testCase.When(NoopCommand{})
+
+		// When
+		tt := testCase.ThenInspectEvents(func(t *testing.T, events []rangedb.Event) {
+			assert.Equal(t, 0, len(events))
+		})
+
+		// Then
+		t.Run("test case", tt)
+	})
+}
+
+func noopDispatcher(_ bdd.Command) {}
+func stubEventDispatcher(store rangedb.Store, events ...rangedb.Event) func(bdd.Command) {
+	return func(_ bdd.Command) {
+		for _, event := range events {
+			_ = store.Save(event, nil)
+		}
+	}
+}
+
+type NoopCommand struct{}
+
+func (n NoopCommand) AggregateID() string   { return "abc" }
+func (n NoopCommand) AggregateType() string { return "noop" }
+func (n NoopCommand) CommandType() string   { return "NoopCommand" }
+
+type NoopEvent struct {
+	ID string
+}
+
+func (n NoopEvent) AggregateID() string   { return n.ID }
+func (n NoopEvent) AggregateType() string { return "noop" }
+func (n NoopEvent) EventType() string     { return "NoopEvent" }
